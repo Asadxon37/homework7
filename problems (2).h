dd7a63c7c7827db4dab34e669d1ace67fe9e17ff
Asadxon37 <m.usmonov@newuu.uz>
2024-11-23 21:19:01 +0500

    Git init

--- problems (2).h
+++ problems (2).h
@@ -14,16 +14,30 @@ return y;
 
 // Problem 3
 int minsNewYear(int *hour, int *min) {
-    
+    int m=14400-hour*60-min;
+    return m;
 }
 
 // Problem 4
 double probability(int *y,int *w) {
-   
+   double prob;
+   if(y>w){prob=(6-y)/6}
+   if(y<w){prob=(6-w)/6}
+   return prob;
 }
 
 // Problem 5
 int presses(int *x) {
-    
+    int k;
+    int t;
+    while(t!=x){
+      for(k=1;k<10;k++){
+        for(int i=1;i<=10000;i+10*k){
+          t=i+10*k;
+          k+=1;
+        }
+      }
+    }
+    return k;
 }
 
