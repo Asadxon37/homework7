8d29899b9e302f416918a9d6c6cd993919ca53d5
Asadxon37 <m.usmonov@newuu.uz>
2024-11-23 23:17:00 +0500

    Git init

--- problems (2).h
+++ problems (2).h
@@ -1,43 +1,41 @@
 
 
 void days2years(int &days, int &years) {
-   int year=*days/365;
-   int day=*days%365;
-  cout<<"Years:"<<year<<"Days:"<<days<<endl;
+   years=days/365;
+   days=days%365;
 }
 // Problem 2
 double func(double *x,double *y) {
-double y=(11*pow(x,3))/3+5;
-return y;
+double *y=(11*(*x)+(*x)+(*x)/3.0+5;
+return *y;
 }
 
 
 // Problem 3
 int minsNewYear(int *hour, int *min) {
-    int m=14400-hour*60-min;
+    int m=1440-(*hour)*60-(*min);
     return m;
 }
 
 // Problem 4
 double probability(int *y,int *w) {
    double prob;
-   if(y>w){prob=(6-y)/6}
-   if(y<w){prob=(6-w)/6}
+   if(*y>*w){prob=(7-(*y))/6;}
+   if(*y<*w){prob=(6-(*w))/6;}
    return prob;
 }
 
 // Problem 5
 int presses(int *x) {
-    int k;
-    int t;
-    while(t!=x){
-      for(k=1;k<10;k++){
-        for(int i=1;i<=10000;i+10*k){
-          t=i+10*k;
-          k+=1;
-        }
+    string num=to_string(*x);
+    int digit=num[0]-'0';
+    int length=num.length();
+    int  count=0;
+    for(int i=1; i<digit;++1){
+      count+=1+2+3+4;
       }
-    }
-    return k;
+      for(int i=0; i<length; ++i){
+        count+=i;
 }
-
+return count;
+}
