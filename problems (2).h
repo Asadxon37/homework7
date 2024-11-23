fe83ddea5b8d59adeb29214b245e8d09c8413375
Asadxon37 <m.usmonov@newuu.uz>
2024-11-23 20:51:42 +0500

    Git init

---
+++ problems (2).h
@@ -0,0 +1,29 @@
+
+
+void days2years(int &days, int &years) {
+   int year=*days/365;
+   int day=*days%365;
+  cout<<"Years:"<<year<<"Days:"<<days<<endl;
+}
+// Problem 2
+double func(double *x,double *y) {
+double y=(11*pow(x,3))/3+5;
+return y;
+}
+
+
+// Problem 3
+int minsNewYear(int *hour, int *min) {
+    
+}
+
+// Problem 4
+double probability(int *y,int *w) {
+   
+}
+
+// Problem 5
+int presses(int *x) {
+    
+}
+
